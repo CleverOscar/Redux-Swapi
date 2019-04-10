501fe9534078ff15a0cb9811d3020eaf2fdfb3b3
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-04-10 18:01:35 -0500

    finished mvp

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
-// import actions
+import { swapiActionCreator } from '../actions/index';
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +10,12 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.swapiActionCreator()
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+    if (this.props.isFetching) {
+      <h1>Fetching</h1>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +25,13 @@ class CharacterListView extends React.Component {
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  isFetching: state.charsReducer.isFetching,
+  error: state.charsReducer.error
+})
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { swapiActionCreator }
 )(CharacterListView);
